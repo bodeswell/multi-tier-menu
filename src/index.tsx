588bcd3298f0f25b8d3fb19615649fe8ff4d3e7b
Avram Score <ascore@gmail.com>
2021-04-19 18:30:17 +0200

    center menu

--- src/index.tsx
+++ src/index.tsx
@@ -153,10 +153,11 @@ export class MultiTierMenu extends Component<any, MenuState> {
             </div>
         ) : null;
 
+        const menuPosition: number = this.state.hoverPosition + 56 - ((secondLayoutItems.length * 34 / 2));
         const secondLayout: JSX.Element | null = secondLayoutItems.length ? (
             <div className={this.props.className ?
                 `${this.props.className}MtmMenu ${styles.mtmMenu}` : styles.mtmMenu}
-                 style={{ top: `${this.state.hoverPosition + 56}px` }}>
+                 style={{ top: `${menuPosition}px` }}>
                 <ul>
                     {secondLayoutItems}
                 </ul>
